c03ad3f802a2b351df3999e1a3d335fae791dc0a
ReedChock <89874483+ReedChock@users.noreply.github.com>
2021-09-28 15:04:26 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -1,45 +1,44 @@
+from adafruit_circuitplayground import cp
 import time
-import array
 import math
-import board
-import digitalio
-
-try:
-    from audiocore import RawSample
-except ImportError:
-    from audioio import RawSample
-
-try:
-    from audioio import AudioOut
-except ImportError:
-    try:
-        from audiopwmio import PWMAudioOut as AudioOut
-    except ImportError:
-        pass  # not always supported by every board!
-
-FREQUENCY = 440  # 440 Hz middle 'A'
-SAMPLERATE = 8000  # 8000 samples/second, recommended!
-
-# Generate one period of sine wav.
-length = SAMPLERATE // FREQUENCY
-sine_wave = array.array("H", [0] * length)
-for i in range(length):
-    sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * (2 ** 15) + 2 ** 15)
-
-# Enable the speaker
-speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
-speaker_enable.direction = digitalio.Direction.OUTPUT
-speaker_enable.value = True
-
-audio = AudioOut(board.SPEAKER)
-sine_wave_sample = RawSample(sine_wave)
-
-# A single sine wave sample is hundredths of a second long. If you set loop=False, it will play
-# a single instance of the sample (a quick burst of sound) and then silence for the rest of the
-# duration of the time.sleep(). If loop=True, it will play the single instance of the sample
-# continuously for the duration of the time.sleep().
-audio.play(sine_wave_sample, loop=True)  # Play the single sine_wave sample continuously...
-time.sleep(1)  # for the duration of the sleep (in seconds)
-audio.stop()  # and then stop.
+cp.pixels.brightness = .05
+grav = 9.80665
 
 
+# while True:
+    # x, y, z = cp.acceleration
+    # r = int(abs(x / grav * 127))
+    # g = int(abs(y / grav * 127))
+    # b = int(abs(z / grav * 127))
+    # cp.pixels.fill([r, g, b])
+    # x_angle = math.asin(x / grav)*180/3.1415926535
+    # y_angle = math.asin(y / grav)*180/3.1415926535
+    # print((x_angle,y_angle))
+    # time.sleep(.05)
+while True:
+    for i in [1,2,3,6,7,8]:
+        x, y, z = cp.acceleration
+        x_angle = math.asin(x / grav)*180/3.1415926535
+        y_angle = math.asin(y / grav)*180/3.1415926535
+#        if ((x_angle) > 50) or ((x_angle) < -50):
+#            cp.pixels[i]=((0, 0, 0))
+        if ((x_angle) > 15) or ((x_angle) < -15):
+            cp.pixels[i]=((255, 0, 0))
+        if ((x_angle) < 15) and ((x_angle) > -15):
+            cp.pixels[i]=((255, 255, 255))
+        if ((x_angle) < 2) and ((x_angle) > -2):
+            cp.pixels[i]=((0, 255, 0))
+        time.sleep(.00000)
+        print((x_angle))
+
+    for h in [0,4,5,9]:
+        x, y, z = cp.acceleration
+        x_angle = math.asin(x / grav)*180/3.1415926535
+        y_angle = math.asin(y / grav)*180/3.1415926535
+        if ((y_angle) > 15) or ((y_angle) < -15):
+            cp.pixels[h]=((255, 0, 0))
+        if ((y_angle) < 15) and ((y_angle) > -15):
+            cp.pixels[h]=((255, 255, 255))
+        if ((y_angle) < 2) and ((y_angle) > -2):
+            cp.pixels[h]=((0, 255, 0))
+        time.sleep(.00000)
